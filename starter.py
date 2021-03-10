58c52987fea95e2817b3fa341038c27f49cf5b45
98io <80382332+98io@users.noreply.github.com>
2021-03-10 08:11:12 +0100

    Create starter.py

---
+++ starter.py
@@ -0,0 +1,16 @@
+# python types
+i = 5
+print(type(i))
+
+# cast 
+print(int(5.5))
+print(int(True))
+
+# tuple: immutable
+x = (0, 1, 2) 
+print(x[0])
+print(x[-1])
+
+# List: mutable
+l = [10, 9, 15]
+l.extend([11, 20])
